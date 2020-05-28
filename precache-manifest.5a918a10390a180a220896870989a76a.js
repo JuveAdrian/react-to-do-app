1505b37203c485c0c2f63de13902af767e9763fe
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-05-28 09:42:26 +0200

    Updates

---
+++ precache-manifest.5a918a10390a180a220896870989a76a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968640739e1bc9d224f2ae7faa510fa4",
+    "url": "/react-to-do-app/index.html"
+  },
+  {
+    "revision": "8b24e8da34d9be6dca7c",
+    "url": "/react-to-do-app/static/css/main.74c5bf8e.chunk.css"
+  },
+  {
+    "revision": "92b119aa3338cc483b0a",
+    "url": "/react-to-do-app/static/js/2.a76f8454.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-do-app/static/js/2.a76f8454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b24e8da34d9be6dca7c",
+    "url": "/react-to-do-app/static/js/main.dc345a28.chunk.js"
+  },
+  {
+    "revision": "c132ba9423eb7af390be",
+    "url": "/react-to-do-app/static/js/runtime-main.d46005d0.js"
+  }
+]);
